650a08ca51f31b0a0509efc6c507713d125c1705
abdulrahman <abdomsdy0@gmail.com>
2022-01-20 10:31:35 -0800

    making the sync code and deploy for start the async code and fetch data from the serve

---
+++ pages/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <Nav />
+    <div class="container">
+      <Hero />
+      <LargeCardDisplay
+        v-for="cardInfo in largeCardInfo"
+        :key="cardInfo.id"
+        :cardsSection="cardInfo"
+      />
+      <smallCardDisplay
+        v-for="cardInfo in smallCardInfo"
+        :key="cardInfo.id"
+        :cardsSection="cardInfo"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import { largeCardSections, smallCardSections } from '@/assets/data.js'
+export default {
+  name: 'IndexPage',
+  data() {
+    return {
+      largeCardInfo: largeCardSections,
+      smallCardInfo: smallCardSections,
+    }
+  },
+}
+</script>
---
+++ pages/my-items.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container">
+    <NuxtLink to="/">Go Back</NuxtLink>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+.container {
+  padding: 5rem 0;
+}
+</style>
